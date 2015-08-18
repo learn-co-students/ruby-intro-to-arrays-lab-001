a5310a9e5c787ff92fdb4a2bec94c340d9e3741d
Biletnikoff <bogere.sanders@gmail.com>
2015-08-18 13:29:22 -0400

    done

---
+++ lib/intro_to_arrays.rb
@@ -0,0 +1,48 @@
+def instantiate_new_array 
+  Array.new
+end
+
+def array_with_two_elements
+  my_array = [ "Zack", "Elli"]
+
+  my_array
+  end
+
+def first_element(taylor_swift)
+    taylor_swift = ["Welcome to New York", "Blank Space", "Style", "Out of The Woods"]
+
+    my_first_array = taylor_swift[0]
+end
+
+def third_element(taylor_swift)
+    taylor_swift = ["Welcome to New York", "Blank Space", "Style", "Out of The Woods"]
+
+    my_first_array = taylor_swift[2]
+
+end
+
+def last_element(taylor_swift)
+    taylor_swift = ["Welcome to New York", "Blank Space", "Style", "Out of The Woods"]
+
+    my_first_array = taylor_swift[-1]
+end
+
+def first_element_with_array_methods(south_east_asia)
+  south_east_asia = ["Thailand", "Cambodia", "Singapore", "Myanmar"]
+
+  my_first_array = south_east_asia.shift
+
+end
+
+def last_element_with_array_methods(south_east_asia)
+  south_east_asia = ["Thailand", "Cambodia", "Singapore", "Myanmar"]
+
+  my_first_array = south_east_asia.pop
+
+end
+
+def length_of_array(programming_languages)
+  programming_languages = ["Ruby", "Javascript", "Python", "C++", "Java", "Lisp", "PHP", "Clojure"]
+
+  programming_languages.length
+end
