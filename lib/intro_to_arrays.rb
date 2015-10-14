ad4c137aefdcce49b92fc1e4b8cb65b0d99e9ae8
github username <github email address>
2015-10-14 16:20:39 -0400

    done

---
+++ lib/intro_to_arrays.rb
@@ -0,0 +1,54 @@
+/instantiating/
+
+def instantiate_new_array 
+	my_new_array = []
+	my_new_array
+end
+
+
+def array_with_two_elements
+	my_two_array = ["one", "two"]
+	my_two_array
+end
+
+
+/indexing/
+
+@taylor_swift = ["Welcome to New York", "Blank Space", "Style", "Out of The Woods"]
+
+
+def first_element(taylor_swift)
+	my_first_element = taylor_swift[0]
+	my_first_element
+end
+
+def third_element(taylor_swift)
+	my_third_element = taylor_swift[2]
+	my_third_element
+end
+
+def last_element(taylor_swift)
+	my_last_element= taylor_swift[3]	
+	my_last_element
+end
+
+@south_east_asia = ["Thailand", "Cambodia", "Singapore", "Myanmar"]
+
+def first_element_with_array_methods(south_east_asia)
+	first_country = south_east_asia.first
+end
+
+def last_element_with_array_methods(south_east_asia)
+	last_country = south_east_asia.last 
+end
+
+@programming_languages = ["Ruby", "Javascript", "Python", "C++", "Java", "Lisp", "PHP", "Clojure"]
+
+def length_of_array(programming_languages)
+	length = programming_languages.length
+end
+
+
+
+
+
