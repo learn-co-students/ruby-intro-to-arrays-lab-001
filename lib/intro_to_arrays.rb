d3fdce669e119a1971f3d7a550288320f7f0b19f
GarrettTaylor <garrett.taylor@icloud.com>
2015-10-16 21:37:01 -0700

    Done

---
+++ lib/intro_to_arrays.rb
@@ -0,0 +1,32 @@
+def instantiate_new_array #creates a new array
+  array = [] #new array
+end
+
+def array_with_two_elements #new array with two elements comma separator
+  array = [0,1]
+end
+
+def first_element(arr) #index with argument-poition 1 = 0
+  first_element = arr[0]
+end
+
+def third_element(arr)
+  my_third_element = arr[2]
+end
+
+def last_element(arr)
+  last_element_array = arr[-1]
+end
+
+def first_element_with_array_methods(arr)
+  first_element_with_array_methods = arr.first
+end
+
+def last_element_with_array_methods(arr)
+  last_element_with_array_methods = arr.last
+end
+
+def length_of_array(arr)
+  length_of_array = arr.length
+end
+
