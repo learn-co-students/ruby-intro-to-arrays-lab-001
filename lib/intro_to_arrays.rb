3b667c6826f8837a218acb99ce1482249f11576d
dremagic <deandre.ellison@gmail.com>
2017-03-16 11:02:33 -0500

    Done.

---
+++ lib/intro_to_arrays.rb
@@ -0,0 +1,33 @@
+def instantiate_new_array
+  my_new_array = []
+end
+
+def array_with_two_elements
+  my_two_array = ["Cats","dogs"]
+end
+
+
+def first_element(indexing)
+  my_first_element = indexing[0]
+
+end
+
+def third_element(indexing)
+  my_third_element = indexing[2]
+end
+
+def last_element(indexing)
+  my_last_element = indexing[-1]
+end
+
+def first_element_with_array_methods(indexing)
+  first_country = indexing.first
+end
+
+def last_element_with_array_methods(indexing)
+  last_country = indexing.last 
+end
+
+def length_of_array(indexing)
+  length = indexing.count 
+end 
